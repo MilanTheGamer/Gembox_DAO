009896f88ec0ce06a9bcadcb37bec0f7567655ea
Typical Engineer <milanthegamer@gmail.com>
2022-01-22 23:31:51 +0400

    contract layout design

--- contracts/GemboxDAO.sol
+++ contracts/GemboxDAO.sol
@@ -2,7 +2,35 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract GemboxDAO {
-  address public owner = msg.sender;
+  address private owner;
+
+  constructor(){
+    owner =  msg.sender;
+  }
   
+  // Get Owner Address
+  function getOwner() public view returns (address) {
+    return owner;
+  }
+
+  // Create Lottery Pool
+  function creatPool() public {
+    
+  }
+  
+  // Join Lottery Pool
+  function joinPool() public {
+    
+  }
+
+  // Claim Winning Prize
+  function claimPrize() public {
+
+  }
+
+  // Choose Winner
+  function chooseWinner() public {
+    
+  }
 
 }
